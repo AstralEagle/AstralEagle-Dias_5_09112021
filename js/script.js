9c07ce8b9c994f0033f85a8d3e315c975ffdd20d
Arthur Dias <arthur.dias@live.fr>
2021-11-09 15:50:55 +0100

    Ajout du fichier JS et ecriture de code pour dynamisé la page d'aceuille

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const tableau = document.getElementById("items");
+
+fetch("http://localhost:3000/api/products")
+.then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+})
+.then(function(value) {
+    console.log(value);
+    for(let item of value){
+        newElement(item);
+    }
+})
+.catch(function(err) {
+    // Une erreur est survenue
+});
+
+
+function newElement(item){
+    console.log(item);
+    const onClic = document.createElement("a");
+    onClic.setAttribute("href","./product.html?id="+item["_id"]);
+    onClic.innerHTML = '<article><img src="'+item["imageUrl"]+'" alt="Image du canapé"><h3 class="productName">'+item["name"]+'</h3><p class="productDescription">'+item["description"]+'</p></article>'
+    tableau.appendChild(onClic);
+}
